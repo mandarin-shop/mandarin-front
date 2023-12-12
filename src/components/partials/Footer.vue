509b50de117182d82f203995e26a5940fce90a06
miss Safiya <maxmudovarruxsora@gmail.com>
2023-12-12 12:28:36 +0500

    footer

--- src/components/partials/Footer.vue
+++ src/components/partials/Footer.vue
@@ -1,15 +1,15 @@
 <script setup></script>
 <template>
   <footer class="footer">
-    <div class="flex justify-between  px-32">
-      <div>
+    <div class="flex  ml-12 gap-8   flex-wrap border-2">
+      <div class="w-72  border-2 ">
           <header class="header">Biz haqimizda</header>
           <div class="mt-3">
             <router-link to="#" class="block header-text">Topshirish punktlari</router-link>
             <router-link to="#" class=" header-text">Vakansiyalar</router-link>
           </div>
       </div>
-      <div>
+      <div class="w-72 border-2">
             
         <header class="header">Foydalanuvchilarga</header>
         <div class="mt-3">
@@ -17,34 +17,33 @@
           <router-link to="#"  class=" header-text">Savol-Javob</router-link>
         </div>
       </div>
-      <div>
+      <div class="w-64 border-2 ">
             
         <header class="header">Tadbirkorlarga</header>
         <div class="mt-3">
           <router-link to="#" class="block header-text">Uzumda soting</router-link>
           <router-link to="#"  class=" header-text">Sotuvchi kabinetiga kirish</router-link></div>
       </div>
-      <div >
+      <div class="w-72 border-2">
             
         <header class="header">Ilovani yuklab olish</header>
-        <div class="mt-4 justify-center flex  items-center ">
+        <div class="mt-4  flex  items-center ">
           <i class='bx bxl-apple '></i> <span class="text-sm pr-5">AppStore</span>
           <i class='bx bxl-play-store  text-blue-500'></i> <span class="text-sm">Google Play</span>
         </div>
       </div>
+
+      <div class="w-72 border-2  ">
+        <header class="header">Mandarin ijtimoiy tarmoqlarda</header>
+          <div class="mt-4  flex  items-center">
+            <i class='bx bx1 bxl-instagram-alt text-pink-500 mx-1 '></i>
+            <i class='bx bx1 bxl-telegram mx-1'></i>
+            <i class='bx bx1 bxl-youtube text-red-700 mx-2'></i>
+            <i class='bx bx1 bxl-facebook-square text-blue-900 mx-2'></i>
+          </div>
+      </div>
     </div>
 
-    <div class="text-right px-36  pt-5">
-      <header class="header">Mandarin ijtimoiy tarmoqlarda</header>
-      <div class="pt-2">
-        <i class='bx bx1 bxl-instagram-alt text-pink-500 mx-1 '></i>
-        <i class='bx bx1 bxl-telegram mx-1'></i>
-        <i class='bx bx1 bxl-youtube text-red-700 mx-2'></i>
-        <i class='bx bx1 bxl-facebook-square text-blue-900 mx-2'></i>
-      </div>
-      
-  
-    </div>
   </footer>
 </template>
 
