c813130ec07f5b6b7acaa17282d12aba55e3385f
Abdunazir <adamjon704@gamil.com>
2023-12-13 11:48:38 +0500

    add grit cards

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,10 @@
-<script setup></script>
+<script setup>
+import gritCard from '../components/card/gritCard.vue';
+</script>
 <template>
   <div class="home">
     <h1>This is an home page</h1>
+    <gritCard></gritCard>
   </div>
 </template>
 
