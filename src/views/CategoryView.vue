ac92338fd4efbdb5ee679dd4e482e3c6befb7769
WebCONuz <muxammadi0799@gmail.com>
2023-12-18 12:28:10 +0500

    Merged Nabieva, ruxsora

--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -3,8 +3,15 @@ import ProductSidebar from "../components/partials/product-sidebar.vue";
 </script>
 <template>
   <div class="category">
-    <ProductSidebar />
+    <div class="container flex pt-4">
+      <ProductSidebar class="w-[300px]" />
+      <div class="content bg-gray-200"></div>
+    </div>
   </div>
 </template>
 
-<style></style>
+<style scoped>
+.content {
+  width: calc(100% - 300px);
+}
+</style>
